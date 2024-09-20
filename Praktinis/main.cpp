ff2c21339c6e0e8381667ade3b2eb8f10c43cee6
Vilius Vilimas <s058019@ad.viko.lt>
2024-09-20 09:39:29 +0300

    Sukurta palyginimo opcija ir pradeta pirkimo funkcija

--- Praktinis/main.cpp
+++ Praktinis/main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 using namespace std;
 
-int main()
-{
-    cout << "Hello, World!" << endl;
+int main() {
     float GBP_Bendras = 0.8593, GBP_Pirkti = 0.8450, GBP_Parduoti = 0.9060;;
     float USD_Bendras = 1.0713, USD_Pirkti = 1.0547, USD_Parduoti = 1.1309;
     float INR_Bendras = 88.8260, INR_Pirkti = 85.2614, INR_Parduoti = 92.8334;
@@ -19,21 +18,45 @@ int main()
 
         switch (choice) {
             case 1:
-
+                cout <<"1 EUR = "<< fixed << setprecision(2) << USD_Bendras << " USD" <<endl;
+                cout <<"1 EUR = "<< fixed << setprecision(2) << GBP_Bendras << " GBP" <<endl;
+                cout <<"1 EUR = "<< fixed << setprecision(2) << INR_Bendras << " INR" <<endl;
                 break;
 
             case 2:
+                while(true) {
+                    cout << "Pasirinkite, kuria valiuta norite pirkti" << endl;
+                    cout << "1. USD" << endl;
+                    cout << "2. GBP" << endl;
+                    cout << "3. INR" << endl;
+                    cout << "4. atgal" << endl;
+                    cin >> choice;
 
-                break;
+                    switch(choice) {
+                        case 1:
+                            float eur;
+                            cout << "Iveskite euru kieki" << endl;
+                            cin >> eur;
+                            cout << eur << " EUR = " << fixed << setprecision(2) << eur * USD_Pirkti << endl;
+                        
+                        case 1:
+                            float eur;
+                        cout << "Iveskite euru kieki" << endl;
+                        cin >> eur;
+                        cout << eur << " EUR = " << fixed << setprecision(2) << eur * USD_Pirkti << endl;
+
+                    }
+                }
+            break;
 
             case 3:
 
-                break:
+                break;
 
                     default: cout<< "Nera tokios opcijos" << endl;
 
+                }
         }
-    }
 
-    return 0;
-}
+        return 0;
+    }
