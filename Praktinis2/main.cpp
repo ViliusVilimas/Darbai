5035d8797630023fc20be446fcf2f25d79461e74
Vilius Vilimas <vilius.vilimas@stud.viko.lt>
2024-10-20 13:41:28 +0300

    Padarytas meniu

---
+++ Praktinis2/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+int main() {
+    int stop=0, opt=0;
+    while (stop == 0) {
+        cout << "Pasirinkite funkcija" << endl;
+        cout << "1. Balse ar ne?" << endl;
+        cout << "2. Didziausias bendras daliklis" << endl;
+        cout << "3. Zaidimas" << endl;
+        cout << "4. Fizz Buzz" << endl;
+        cout << "5. Baigti programa" << endl;
+        cin>>opt;
+
+        switch (opt) {
+            case 1: continue;
+
+            case 2: continue;
+
+            case 3: continue;
+
+            continue;
+        }
+
+
+    }
+
+
+
+    return 0;
+}
