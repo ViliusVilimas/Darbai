22f183ef18f5a700673f0c0f0b1e6f1b82df72e3
Vilius Vilimas <s058019@ad.viko.lt>
2024-11-15 08:55:24 +0200

    Kuriamas meniu

---
+++ Prakt3/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int down=0, par, down2=0, down3=0;
+    while (down == 0) {
+        cout << "Kokia srifavimo/desrifavimo sistema norite naudoti?" << endl;
+        cout << "1. Vigeneres algoritma" << endl;
+        cout << "2. ASCII koduote" << endl;
+        cin>>par;
+
+        switch (par) {
+            case 1: {
+                while (down2==0) {
+                    
+                }
+
+            }
+            case 2: {
+
+            }
+
+            default: {
+
+            }
+
+
+        }
+        return 0;
+    }
+}
