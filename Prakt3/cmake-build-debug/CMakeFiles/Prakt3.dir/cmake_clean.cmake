dd5e708993c1ac149728368b84d88c371ccd24e8
Vilius Vilimas <vilius.vilimas@stud.viko.lt>
2024-11-21 08:35:46 +0200

    Daromas pirmasis algoritmas

---
+++ Prakt3/cmake-build-debug/CMakeFiles/Prakt3.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Prakt3.dir/main.cpp.obj"
+  "CMakeFiles/Prakt3.dir/main.cpp.obj.d"
+  "Prakt3.exe"
+  "Prakt3.exe.manifest"
+  "Prakt3.pdb"
+  "libPrakt3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Prakt3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Prakt3/main.cpp
+++ Prakt3/main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
-#include <iomanip>
+#include <string>
 using namespace std;
 
+string cipher(string word, string key);
+string decipher(string word, string key);
 
+    const char abc[]={'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 int main() {
+    string key="KEY";
+
     int down=0;
     while (down == 0) {
         int par, down2=0, down3=0;
-        cout << "Kokia srifavimo/desrifavimo sistema norite naudoti? (Naudojama tik angliska abecele)" << endl;
+        cout << "Kokia srifravimo/desrifravimo sistema norite naudoti? (Naudojama tik angliska abecele)" << endl;
         cout << "1. Vigeneres algoritma" << endl;
         cout << "2. ASCII koduote" << endl;
         cout << "3. Baigti programa" << endl;
@@ -16,18 +21,25 @@ int main() {
         switch (par) {
             case 1: {
                 while (down2==0) {
-                    char abc[][]={{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}, {1,2,3,4,5,6,7,8,9,} };
                     cout << "Ar norite srifruoti ar desrifruoti?" << endl;
-                    cout << "1. srifuoti" << endl;
-                    cout << "2. desrifuoti" << endl;
+                    cout << "1. srifruoti" << endl;
+                    cout << "2. desrifruoti" << endl;
                     cout << "3. Atgal" << endl;
                     cin>>par;
                     switch (par) {
                         case 1: {
+                            string word;
+                            cout << "Irasykite, ka norite srifruoti" << endl;
+                            cin>>word;
+                            cout << cipher(word, key) << endl;
 
                             break;
                         }
                         case 2: {
+                            string word;
+                            cout << "Irasykite, ka norite desrifruoti" << endl;
+                            cin>>word;
+                            cout << decipher(word, key) << endl;
 
                             break;
                         }
@@ -44,8 +56,8 @@ int main() {
             case 2: {
                 while (down3==0) {
                     cout << "Ar norite srifruoti ar desrifruoti?" << endl;
-                    cout << "1. srifuoti" << endl;
-                    cout << "2. desrifuoti" << endl;
+                    cout << "1. srifruoti" << endl;
+                    cout << "2. desrifruoti" << endl;
                     cout << "3. Atgal" << endl;
                     cin>>par;
                     switch (par) {
@@ -78,4 +90,46 @@ int main() {
         }
     }
     return 0;
+}
+
+string cipher(string word, string key) {
+    int x=word.length(), k;
+    string rez, aword;
+    for (int i=0;; i++) {
+        if (i==key.length()) {
+            i=0;
+        }
+        rez+=key[i];
+        if (rez.length()==word.length()) {
+            break;
+        }
+    }
+    for (int i=0; i<x; i++) {
+        cout << rez[i] <<" "<<word[i]<<" "<<endl;
+        k=(toupper(word[i])+rez[i] * 2 + 26) % 26 + 'A';
+        aword+=abc[k];
+
+
+    }
+    return aword;
+}
+string decipher(string word, string key) {
+    int x=word.length(), k;
+    string rez, aword;
+    for (int i=0;; i++) {
+        if (i==key.length()) {
+            i=0;
+        }
+        rez+=key[i];
+        if (rez.length()==word.length()) {
+            break;
+        }
+    }
+    for (int i=0; i<x; i++) {
+        k=(word[i]-rez[i]);
+        aword+=abc[k];
+        cout<<aword<<endl;
+
+    }
+    return aword;
 }
